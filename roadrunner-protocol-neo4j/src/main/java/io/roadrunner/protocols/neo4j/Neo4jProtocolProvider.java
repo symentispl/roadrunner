6387424a32bdc9b303d0b4e66288df87283410f2
Jarosław Pałka <jaroslaw.palka@symentis.pl>
2025-04-28 21:09:56 +0200

    modules modules modules

--- roadrunner-protocol-neo4j/src/main/java/io/roadrunner/protocols/neo4j/Neo4jProtocolProvider.java
+++ roadrunner-protocol-neo4j/src/main/java/io/roadrunner/protocols/neo4j/Neo4jProtocolProvider.java
@@ -21,10 +21,22 @@ import io.roadrunner.protocols.spi.ProtocolProvider;
 import org.neo4j.driver.AuthTokens;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
 import picocli.CommandLine;
+
+import java.net.URI;
 
 @CommandLine.Command(description = "Neo4j protocol provider")
 public class Neo4jProtocolProvider implements ProtocolProvider {
+
+    @CommandLine.Parameters(paramLabel = "uri", description = "Neo4j URI")
+    private URI uri;
+
+    @CommandLine.Option(names = "--username", description = "Neo4j username")
+    private String username = "neo4j";
+
+    @CommandLine.Option(names = "--password", description = "Neo4j password")
+    private String password = "neo4j";
 
     private Driver driver;
 
@@ -35,11 +47,17 @@ public class Neo4jProtocolProvider implements ProtocolProvider {
 
     @Override
     public Protocol newProtocol() {
-        driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "password"));
+        driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password));
         return new Protocol() {
+
+            private Session session = driver.session();
+
             @Override
             public ProtocolResponse execute() {
-                return driver.session().executeRead(tx -> tx.run("RETURN 1").single().get(0).asInt());
+                var startTime = System.nanoTime();
+                var response = session.executeRead(tx -> tx.run("RETURN 1")).consume();
+                var stopTime = System.nanoTime();
+                return ProtocolResponse.response(startTime, stopTime, response);
             }
         };
     }
