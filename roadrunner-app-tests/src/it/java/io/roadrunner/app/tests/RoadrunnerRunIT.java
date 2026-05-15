bdfa37200671e0437508b7e21aaa7005834ff113
Jarosław Pałka <jaroslaw.palka@symentis.pl>
2026-05-15 17:05:58 +0200

    fixes after rebase

--- roadrunner-app-tests/src/it/java/io/roadrunner/app/tests/RoadrunnerRunIT.java
+++ roadrunner-app-tests/src/it/java/io/roadrunner/app/tests/RoadrunnerRunIT.java
@@ -49,7 +49,7 @@ public class RoadrunnerRunIT {
     void closedWorldRunWithoutPauseDetectors(@TempDir Path outputDir) throws Exception {
         // Regression: passing no --pause-detectors used to NPE because the EnumSet field was null.
         var process = new ProcessBuilder(
-                        ROADRUNNER_BIN.toString(),
+                        roadrunnerBin.toString(),
                         "run",
                         "-n",
                         "10",
